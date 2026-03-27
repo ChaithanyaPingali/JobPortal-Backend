ea086c9b6ea1d695e59c31fb4a5ce9107bdef854
Chaithanya Pingali <pingali1998@gmail.com>
2026-03-27 13:22:32 -0500

    gitignore

--- src/main/java/com/eazybytes/jobportal/JobportalApplication.java
+++ src/main/java/com/eazybytes/jobportal/JobportalApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobportalApplication {
 
-	public static void main(String[] args) {
+	 static void main(String[] args) {
 		SpringApplication.run(JobportalApplication.class, args);
 	}
 

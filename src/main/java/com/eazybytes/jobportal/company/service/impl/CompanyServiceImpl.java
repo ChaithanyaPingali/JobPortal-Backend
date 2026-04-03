4f4946f60ec39ced771ab9a78328a247de55ce51
Chaithanya Pingali <pingali1998@gmail.com>
2026-04-03 18:54:33 -0500

    Implemented AOP

--- src/main/java/com/eazybytes/jobportal/company/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/eazybytes/jobportal/company/service/impl/CompanyServiceImpl.java
@@ -8,6 +8,7 @@ import com.eazybytes.jobportal.entity.Job;
 import com.eazybytes.jobportal.repository.CompanyRepository;
 import com.eazybytes.jobportal.company.service.ICompanyService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
